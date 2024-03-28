3bbaffa45b6b62ecdb3323d3c5ac29cd3e6d9a49
codeclanstudent <codeclanstudent@codeclanstudents-MacBook-Pro.local>
2024-03-28 17:44:34 +0000

    New site

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,35 @@
 // src/components/Home.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
+// Array of image URLs for the carousel
+const images = [
+    '/media/pic.jpg',
+    '/media/pic2.jpg',
+    '/media/pic3.jpg',
+    '/media/pic4.jpg',
+    '/media/pic5.jpg',
+  ];
+  
+
 const Home = ({ togglePlayPause, isPlaying, playNextSong, hasStartedPlaying, setHasStartedPlaying }) => {
   const years = [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, "Videos"];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex(currentIndex => (currentIndex + 1) % images.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(intervalId); // Clean up on component unmount
+  }, []);
 
   const handlePlaySong = () => {
     if (!hasStartedPlaying) {
       setHasStartedPlaying(true); // Mark as started
       playNextSong(); // Play the song
+      if (!isPlaying) togglePlayPause(); // Ensure the song plays
     } else {
       playNextSong(); // Play the next song
     }
@@ -18,6 +38,7 @@ const Home = ({ togglePlayPause, isPlaying, playNextSong, hasStartedPlaying, set
   return (
     <div className="home-container">
       <h1 className="birthday-title">Happy Birthday Madelyn!</h1>
+      <img src={images[currentImageIndex]} alt="Slideshow" className="homepage-slideshow" />
       <button onClick={handlePlaySong} className="music-button">
         {hasStartedPlaying ? 'Play Next Song' : 'Play Song'}
       </button>
@@ -31,7 +52,6 @@ const Home = ({ togglePlayPause, isPlaying, playNextSong, hasStartedPlaying, set
           <button className="year-button">{year}</button>
         </Link>
       ))}
-      {/* Additional content */}
     </div>
   );
 };
